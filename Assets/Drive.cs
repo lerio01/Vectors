28d076abe320aabf17f07c7d0559561e27e15628
lerio01 <kovalenkolera11@gmail.com>
2024-04-21 00:16:37 +0300

    first commit
    Add project

---
+++ Assets/Drive.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+public class Drive : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+   
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+    }
+}
